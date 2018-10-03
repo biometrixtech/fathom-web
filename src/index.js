04c390dc9ae943b97d54db5600bd930cd659b622
Mazen Chami <mazenchami@gmail.com>
2018-10-03 11:14:22 -0400

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
+import 'semantic-ui-css/semantic.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
